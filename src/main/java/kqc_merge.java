c9a14570ff94ebab1539d08f3abfbf6f39c652a3
康期朝 <kang.qizhao@trs.com.cn>
2017-02-22 09:29:54 +0800

    调整结构

--- src/main/java/kqc_merge.java
+++ src/main/java/kqc_merge.java
@@ -3,37 +3,40 @@
  */
 public class kqc_merge {
 
-
-    public static String merge(String... Data) {
-        int iDataCount = Data.length;
-        String sReturnData = Data[0];
-        for (int i = 1; i < iDataCount; i++) {
-            String sTempData=Data[i];
-            //获得短字符串的长度作为循环次数
-            int iShortCount=sReturnData.length()>sTempData.length()?sTempData.length():sReturnData.length();
-            //获得需要拼接的字符串
-            String sShortData=sReturnData.length()>sTempData.length()?"":sTempData.substring(iShortCount);
-            //进行循环
-            for (int h=0;h<iShortCount;h++){
-                String sReturnDataH=sReturnData.substring(h,h+1);
-                String sTempDataH=sTempData.substring(h,h+1);
-                if (!sReturnDataH.equals(sTempDataH)){
-                    sReturnData=sReturnData.substring(0,h)+"1"+sReturnData.substring(h+1);
-                }
-
-            }
-            sReturnData+=sShortData;
-            //进行拼接
-
-        }
-        return sReturnData;
-
-    }
-
     public static void main(String[] args) {
         String[] arrData = {"1000", "11111100", "1001010100"};
         String a=merge(arrData);
         System.out.println(a);
+
+    }
+    public static String merge(String... Data) {
+        //数据源长度
+        int iDataCount = Data.length;
+        //第一个数据
+        String sReturnData = Data[0];
+        for (int i = 1; i < iDataCount; i++) {
+            //依次进行比较的数据
+            String sTempData=Data[i];
+            //获得短字符串的长度作为循环次数
+            int iShortCount=sReturnData.length()>sTempData.length()?sTempData.length():sReturnData.length();
+            //较长数据的多出来部分直接进行拼接在结果，不需要比较
+            String sShortData=sReturnData.length()>sTempData.length()?"":sTempData.substring(iShortCount);
+            //进行循环
+            for (int h=0;h<iShortCount;h++){
+                //两条数据依次比较
+                String sReturnDataH=sReturnData.substring(h,h+1);
+                String sTempDataH=sTempData.substring(h,h+1);
+                if (!sReturnDataH.equals(sTempDataH)){
+                    //拼接比较结果
+                    sReturnData=sReturnData.substring(0,h)+"1"+sReturnData.substring(h+1);
+                }
+
+            }
+            //给比较的结果拼接上长数据多出来的部分
+            sReturnData+=sShortData;
+
+        }
+        return sReturnData;
 
     }
 }
